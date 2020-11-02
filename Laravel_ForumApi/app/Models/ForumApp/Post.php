1cad16557aad98b70527bcc7b5b6088aac3c3367
Eugene <61174287+eugene18-art@users.noreply.github.com>
2020-11-02 16:48:32 +0800

    :heavy_plus_sign: init laravel version

---
+++ Laravel_ForumApi/app/Models/ForumApp/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\ForumApp;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'content',
+    ];
+}
---
+++ Laravel_ForumApi/app/Models/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The global attributes for user
+     */
+    public static $MEMBER_TYPES = [
+        'regular'=>'regular', 
+        'admin'=>'admin'
+    ];
+    public static $GENDER_TYPES = [
+        'male'=>'male',
+        'female'=>'female'
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'types',
+        'birth_date',
+        'birth_place',
+        'gender'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
